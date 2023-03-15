13e63ae46397bb1a7e46db806b6443564ff1da9f
hunjixin <1084400399@qq.com>
2023-03-15 16:14:04 +0800

    feat: modify front

--- web/ui/src/api/dto/node-library.ts
+++ web/ui/src/api/dto/node-library.ts
@@ -21,17 +21,10 @@ export interface INodeVo extends Readonly<{
   category: string
   description: string
   path: string
-  properties:[]IPropertyDto
+  isAnnotateOut: boolean
+  properties: IPropertyDto[]
+  SvcProperties: IPropertyDto[]
+  Out?:IPropertyDto
   deprecated: boolean;
 }> {
-}
-
-
-type PluginOut struct {
-  BaseTime
-  PluginInfo
-  Properties    []Property `json:"properties"`
-  IsAnnotateOut bool       `json:"isAnnotateOut"`
-  SvcProperties []Property `json:"svcProperties"`
-  Out           *Property  `json:"out"`
 }
