06c2f496ad2fc57ffe28ed79e76a452fd27adfab
andream7 <2236407598@qq.com>
2023-04-20 13:09:14 +0800

    feat: update diff-token

--- exec/plugins/diff-permissions-token/diff-permissions-token.go
+++ exec/plugins/diff-permissions-token/diff-permissions-token.go
@@ -3,8 +3,15 @@ package main
 import (
 	"context"
 	"fmt"
+
+	"github.com/filecoin-project/go-address"
+	"github.com/filecoin-project/go-state-types/abi"
 	"github.com/filecoin-project/venus-auth/auth"
 	"github.com/filecoin-project/venus-auth/jwtclient"
+	chain "github.com/filecoin-project/venus/venus-shared/api/chain/v1"
+	"github.com/filecoin-project/venus/venus-shared/api/wallet"
+	types2 "github.com/filecoin-project/venus/venus-shared/types"
+	vTypes "github.com/filecoin-project/venus/venus-shared/types"
 	"github.com/hunjixin/brightbird/env"
 	"github.com/hunjixin/brightbird/types"
 	"github.com/hunjixin/brightbird/utils"
@@ -58,17 +65,81 @@ func Exec(ctx context.Context, params TestCaseParams) error {
 	}
 	fmt.Println(token)
 
-	switch params.Params.Permission {
-	case "admin":
-		fmt.Println("num is 1")
-	case "sign":
-		fmt.Println("num is 2")
-	case "write":
-		fmt.Println("num is 3")
-	case "read":
-		fmt.Println("num is 4")
-	default:
-		fmt.Println("Incorrect permission type")
+	permission, err := checkPermission(ctx, endpoint.ToMultiAddr(), token)
+	if err != nil {
+		return err
+	}
+	if permission != params.Params.Permission {
+		return err
 	}
 	return nil
+}
+
+func checkPermission(ctx context.Context, addr string, token string) (string, error) {
+	chainRpc, closer, err := chain.DialFullNodeRPC(ctx, addr, token, nil)
+	if err != nil {
+		return "", err
+	}
+	defer closer()
+
+	walletAddr, err := createWallet(ctx, addr, token)
+	if err != nil {
+		return "", err
+	}
+
+	chainHead, err := chainRpc.ChainHead(ctx)
+	read := err == nil && chainHead != nil
+
+	writeErr := chainRpc.MpoolPublishByAddr(ctx, walletAddr)
+	write := writeErr == nil
+
+	msg := types2.Message{
+		From:       walletAddr,
+		To:         walletAddr,
+		Value:      abi.NewTokenAmount(0),
+		GasFeeCap:  abi.NewTokenAmount(0),
+		GasPremium: abi.NewTokenAmount(0),
+	}
+
+	signedMsg, signErr := chainRpc.WalletSignMessage(ctx, walletAddr, &msg)
+	sign := signErr == nil && signedMsg != nil
+
+	adminAddrs := chainRpc.WalletAddresses(ctx)
+	admin := adminAddrs != nil && len(adminAddrs) > 0
+
+	if read && !write && !sign && !admin {
+		return "read", nil
+	}
+	if !read && write && !sign && !admin {
+		return "write", nil
+	}
+	if !read && !write && sign && !admin {
+		return "sign", nil
+	}
+	if !read && !write && !sign && admin {
+		return "admin", nil
+	}
+
+	return "", nil
+}
+
+func createWallet(ctx context.Context, addr string, token string) (address.Address, error) {
+
+	walletRpc, closer, err := wallet.DialIFullAPIRPC(ctx, addr, token, nil)
+	if err != nil {
+		return address.Undef, err
+	}
+	defer closer()
+
+	password := "123456"
+	err = walletRpc.SetPassword(ctx, password)
+	if err != nil {
+		return address.Undef, err
+	}
+	walletAddr, err := walletRpc.WalletNew(ctx, vTypes.KTBLS)
+	if err != nil {
+		return address.Undef, err
+	}
+	fmt.Println("wallet:", walletAddr)
+	return walletAddr, nil
 }
