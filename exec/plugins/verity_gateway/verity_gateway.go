b7f1469fe3373398bf8eb5fc793301d3da1c03bd
andream7 <2236407598@qq.com>
2023-04-23 14:38:46 +0800

    feat: update add verity gateway

---
+++ exec/plugins/verity_gateway/verity_gateway.go
@@ -0,0 +1,155 @@
+package main
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"github.com/filecoin-project/go-address"
+	"github.com/filecoin-project/venus-auth/auth"
+	"github.com/filecoin-project/venus-auth/jwtclient"
+	v2API "github.com/filecoin-project/venus/venus-shared/api/gateway/v2"
+	"github.com/filecoin-project/venus/venus-shared/api/wallet"
+	vTypes "github.com/filecoin-project/venus/venus-shared/types"
+	"github.com/hunjixin/brightbird/env"
+	"github.com/hunjixin/brightbird/types"
+	"github.com/hunjixin/brightbird/utils"
+	"github.com/hunjixin/brightbird/version"
+	"go.uber.org/fx"
+)
+
+var Info = types.PluginInfo{
+	Name:        "verity_gateway",
+	Version:     version.Version(),
+	Category:    types.TestExec,
+	Description: "verity gateway if normal",
+}
+
+type TestCaseParams struct {
+	fx.In
+	Params struct {
+		Permission string `json:"permission"`
+	} `optional:"true"`
+	AdminToken   types.AdminToken
+	K8sEnv       *env.K8sEnvDeployer       `json:"-"`
+	VenusAuth    env.IVenusAuthDeployer    `json:"-"`
+	VenusGateway env.IVenusGatewayDeployer `json:"-"`
+	VenusWallet  env.IVenusWalletDeployer  `json:"-"`
+}
+
+func Exec(ctx context.Context, params TestCaseParams) error {
+	authToken, err := CreateAuthToken(ctx, params)
+	if err != nil {
+		fmt.Printf("create wallet failed: %v\n", err)
+		return err
+	}
+
+	_, err = CreateWallet(ctx, params)
+	if err != nil {
+		fmt.Printf("create miner failed: %v\n", err)
+		return err
+	}
+
+	err = GetWalletInfo(ctx, params, authToken)
+	if err != nil {
+		fmt.Printf("get miner info failed: %v\n", err)
+		return err
+	}
+
+	return nil
+
+}
+
+func CreateAuthToken(ctx context.Context, params TestCaseParams) (adminToken string, err error) {
+	endpoint := params.VenusAuth.SvcEndpoint()
+	if env.Debug {
+		var err error
+		endpoint, err = params.K8sEnv.PortForwardPod(ctx, params.VenusAuth.Pods()[0].GetName(), int(params.VenusAuth.Svc().Spec.Ports[0].Port))
+		if err != nil {
+			return "", err
+		}
+	}
+
+	authAPIClient, err := jwtclient.NewAuthClient(endpoint.ToHttp(), string(params.AdminToken))
+	if err != nil {
+		return "", err
+	}
+	_, err = authAPIClient.CreateUser(ctx, &auth.CreateUserRequest{
+		Name:    "admin",
+		Comment: utils.StringPtr("comment admin"),
+		State:   0,
+	})
+	if err != nil {
+		return "", err
+	}
+
+	adminToken, err = authAPIClient.GenerateToken(ctx, "admin", "admin", "")
+	if err != nil {
+		return "", err
+	}
+
+	return adminToken, nil
+}
+
+func CreateWallet(ctx context.Context, params TestCaseParams) (address.Address, error) {
+	walletToken, err := env.ReadWalletToken(ctx, params.K8sEnv, params.VenusWallet.Pods()[0].GetName())
+	if err != nil {
+		return address.Undef, fmt.Errorf("read wallet token failed: %w\n", err)
+	}
+
+	endpoint := params.VenusWallet.SvcEndpoint()
+	if env.Debug {
+		var err error
+		endpoint, err = params.K8sEnv.PortForwardPod(ctx, params.VenusWallet.Pods()[0].GetName(), int(params.VenusWallet.Svc().Spec.Ports[0].Port))
+		if err != nil {
+			return address.Undef, fmt.Errorf("port forward failed: %w\n", err)
+		}
+	}
+
+	walletRpc, closer, err := wallet.DialIFullAPIRPC(ctx, endpoint.ToMultiAddr(), walletToken, nil)
+	if err != nil {
+		return address.Undef, fmt.Errorf("dial iFullAPI rpc failed: %w\n", err)
+	}
+	defer closer()
+
+	password := "123456"
+	err = walletRpc.SetPassword(ctx, password)
+	if err != nil {
+		return address.Undef, fmt.Errorf("set password failed: %w\n", err)
+	}
+
+	walletAddr, err := walletRpc.WalletNew(ctx, vTypes.KTBLS)
+	if err != nil {
+		return address.Undef, fmt.Errorf("create wallet failed: %w\n", err)
+	}
+	fmt.Printf("wallet: %v\n", walletAddr)
+
+	return walletAddr, nil
+}
+
+func GetWalletInfo(ctx context.Context, params TestCaseParams, authToken string) error {
+	endpoint := params.VenusAuth.SvcEndpoint()
+	if env.Debug {
+		var err error
+		endpoint, err = params.K8sEnv.PortForwardPod(ctx, params.VenusAuth.Pods()[0].GetName(), int(params.VenusAuth.Svc().Spec.Ports[0].Port))
+		if err != nil {
+			return err
+		}
+	}
+
+	api, closer, err := v2API.DialIGatewayRPC(ctx, endpoint.ToHttp(), authToken, nil)
+	if err != nil {
+		return err
+	}
+	defer closer()
+
+	wallets, err := api.ListWalletInfo(ctx)
+	if err != nil {
+		return err
+	}
+	minersBytes, err := json.MarshalIndent(wallets, " ", "\t")
+	if err != nil {
+		return err
+	}
+	fmt.Println(string(minersBytes))
+	return nil
+}
