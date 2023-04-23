bb966fae78a81c57ade15c21930c7957ee75b37f
andream7 <2236407598@qq.com>
2023-04-23 10:12:12 +0800

    feat: update add miner

--- exec/plugins/add-miner/add-miner.go
+++ exec/plugins/add-miner/add-miner.go
@@ -2,12 +2,15 @@ package main
 
 import (
 	"context"
-	"errors"
 	"fmt"
+	"github.com/filecoin-project/go-address"
+	"github.com/filecoin-project/venus/venus-shared/api/wallet"
+	vTypes "github.com/filecoin-project/venus/venus-shared/types"
 	"github.com/hunjixin/brightbird/env"
 	"github.com/hunjixin/brightbird/types"
 	"github.com/hunjixin/brightbird/version"
 	"go.uber.org/fx"
+	"math/rand"
 )
 
 var Info = types.PluginInfo{
@@ -19,45 +22,31 @@ var Info = types.PluginInfo{
 
 type TestCaseParams struct {
 	fx.In
-	Params struct {
-		From       string `json:"from"`
-		Owner      string `json:"owner"`
-		Worker     string `json:"worker"`
-		SectorSize string `json:"sectorSize"`
-		Peer       string `json:"peer"`
-		Multiaddr  string `json:"multiaddr"`
-		Exid       string `json:"exid"`
-	} `optional:"true"`
 	AdminToken                 types.AdminToken
 	K8sEnv                     *env.K8sEnvDeployer             `json:"-"`
+	VenusWallet                env.IVenusWalletDeployer        `json:"-"`
 	VenusSectorManagerDeployer env.IVenusSectorManagerDeployer `json:"-"`
 }
 
 func Exec(ctx context.Context, params TestCaseParams) error {
+
+	walletAddr, err := createWallet(ctx, params)
+	if err != nil {
+		return err
+	}
+
 	cmd := []string{
 		"./venus-sector-manager",
 		"util",
 		"miner",
 		"create",
-	}
-	switch {
-	case params.Params.From != "":
-		cmd = append(cmd, "--from="+params.Params.From)
-	case params.Params.Owner != "":
-		cmd = append(cmd, "--owner="+params.Params.Owner)
-	case params.Params.Worker != "":
-		cmd = append(cmd, "--worker="+params.Params.Worker)
-	case params.Params.SectorSize != "":
-		cmd = append(cmd, "--peer="+params.Params.Peer)
-	case params.Params.Multiaddr != "":
-		cmd = append(cmd, "--multiaddr="+params.Params.Multiaddr)
-	case params.Params.Exid != "":
-		cmd = append(cmd, "--exid"+params.Params.Exid)
-	default:
-		return errors.New("parameter err")
+		"--sector-size=32GiB",
+		"--exid=" + string(rune(rand.Intn(1000))),
 	}
 
+	cmd = append(cmd, "--from="+walletAddr.String())
 	fmt.Println(cmd)
+
 	minerAddr, err := params.K8sEnv.ExecRemoteCmd(ctx, params.VenusSectorManagerDeployer.Pods()[0].GetName(), cmd)
 	if err != nil {
 		return err
@@ -76,4 +65,40 @@ func Exec(ctx context.Context, params TestCaseParams) error {
 	}
 	fmt.Println(minerInfo)
 	return nil
+}
+
+func createWallet(ctx context.Context, params TestCaseParams) (address.Address, error) {
+
+	walletToken, err := env.ReadWalletToken(ctx, params.K8sEnv, params.VenusWallet.Pods()[0].GetName())
+	if err != nil {
+		fmt.Println("请提供venus_wallet组件")
+		return address.Undef, err
+	}
+
+	endpoint := params.VenusWallet.SvcEndpoint()
+	if env.Debug {
+		var err error
+		endpoint, err = params.K8sEnv.PortForwardPod(ctx, params.VenusWallet.Pods()[0].GetName(), int(params.VenusWallet.Svc().Spec.Ports[0].Port))
+		if err != nil {
+			return address.Undef, err
+		}
+	}
+
+	walletRpc, closer, err := wallet.DialIFullAPIRPC(ctx, endpoint.ToMultiAddr(), walletToken, nil)
+	if err != nil {
+		return address.Undef, err
+	}
+	defer closer()
+
+	password := "123456"
+	err = walletRpc.SetPassword(ctx, password)
+	if err != nil {
+		return address.Undef, err
+	}
+	walletAddr, err := walletRpc.WalletNew(ctx, vTypes.KTBLS)
+	if err != nil {
+		return address.Undef, err
+	}
+	fmt.Println("wallet:", walletAddr)
+	return walletAddr, nil
 }
