a6b4acdc070a8c95d7dfced180b303f487cfa84c
andream7 <2236407598@qq.com>
2023-04-26 15:25:09 +0800

    feat: update actor set-addrs

--- exec/plugins/actor-set-addrs/actor-set-addrs.go
+++ exec/plugins/actor-set-addrs/actor-set-addrs.go
@@ -3,11 +3,15 @@ package main
 import (
 	"context"
 	"fmt"
+	"github.com/filecoin-project/go-address"
 	marketapi "github.com/filecoin-project/venus/venus-shared/api/market/v1"
+	"github.com/filecoin-project/venus/venus-shared/api/wallet"
+	vTypes "github.com/filecoin-project/venus/venus-shared/types"
 	"github.com/hunjixin/brightbird/env"
 	"github.com/hunjixin/brightbird/types"
 	"github.com/hunjixin/brightbird/version"
 	"go.uber.org/fx"
+	"math/rand"
 )
 
 var Info = types.PluginInfo{
@@ -19,10 +23,13 @@ var Info = types.PluginInfo{
 
 type TestCaseParams struct {
 	fx.In
-	AdminToken  types.AdminToken
-	K8sEnv      *env.K8sEnvDeployer      `json:"-"`
-	VenusAuth   env.IVenusAuthDeployer   `json:"-"`
-	VenusMarket env.IVenusMarketDeployer `json:"-"`
+	AdminToken                 types.AdminToken
+	K8sEnv                     *env.K8sEnvDeployer             `json:"-"`
+	VenusAuth                  env.IVenusAuthDeployer          `json:"-"`
+	VenusMarket                env.IVenusMarketDeployer        `json:"-"`
+	VenusWallet                env.IVenusWalletDeployer        `json:"-" svcname:"Wallet"`
+	VenusMiner                 env.IVenusMinerDeployer         `json:"-"`
+	VenusSectorManagerDeployer env.IVenusSectorManagerDeployer `json:"-"`
 }
 
 func Exec(ctx context.Context, params TestCaseParams) error {
@@ -33,10 +40,111 @@ func Exec(ctx context.Context, params TestCaseParams) error {
 		return err
 	}
 	fmt.Printf("market net listen is: %v\n", listenAddress)
+
+	walletAddr, err := CreateWallet(ctx, params)
+	if err != nil {
+		fmt.Printf("create wallet failed: %v\n", err)
+		return err
+	}
+
+	minerAddr, err := CreateMiner(ctx, params, walletAddr)
+	if err != nil {
+		fmt.Printf("create miner failed: %v\n", err)
+		return err
+	}
+
+	messageId, err := SetActorAddr(ctx, params, minerAddr)
+	if err != nil {
+		fmt.Printf("set actor address failed: %v\n", err)
+		return err
+	}
+	fmt.Printf("set actor address message id is: %v\n", messageId)
+
 	return nil
 }
 
 func marketListen(ctx context.Context, params TestCaseParams) (string, error) {
+	endpoint := params.VenusMarket.SvcEndpoint()
+	if env.Debug {
+		var err error
+		endpoint, err = params.K8sEnv.PortForwardPod(ctx, params.VenusMarket.Pods()[0].GetName(), int(params.VenusMarket.Svc().Spec.Ports[0].Port))
+		if err != nil {
+			return "", err
+		}
+	}
+	client, closer, err := marketapi.NewIMarketRPC(ctx, endpoint.ToHttp(), nil)
+	if err != nil {
+		return "", err
+	}
+	defer closer()
+
+	addrs, err := client.NetAddrsListen(ctx)
+	if err != nil {
+		return "", nil
+	}
+
+	for _, peer := range addrs.Addrs {
+		fmt.Printf("%s/p2p/%s\n", peer, addrs.ID)
+	}
+	return "", err
+}
+
+func CreateWallet(ctx context.Context, params TestCaseParams) (address.Address, error) {
+	walletToken, err := env.ReadWalletToken(ctx, params.K8sEnv, params.VenusWallet.Pods()[0].GetName())
+	if err != nil {
+		return address.Undef, fmt.Errorf("read wallet token failed: %w\n", err)
+	}
+
+	endpoint := params.VenusWallet.SvcEndpoint()
+	if env.Debug {
+		var err error
+		endpoint, err = params.K8sEnv.PortForwardPod(ctx, params.VenusWallet.Pods()[0].GetName(), int(params.VenusWallet.Svc().Spec.Ports[0].Port))
+		if err != nil {
+			return address.Undef, fmt.Errorf("port forward failed: %w\n", err)
+		}
+	}
+
+	walletRpc, closer, err := wallet.DialIFullAPIRPC(ctx, endpoint.ToMultiAddr(), walletToken, nil)
+	if err != nil {
+		return address.Undef, fmt.Errorf("dial iFullAPI rpc failed: %w\n", err)
+	}
+	defer closer()
+
+	password := "123456"
+	err = walletRpc.SetPassword(ctx, password)
+	if err != nil {
+		return address.Undef, fmt.Errorf("set password failed: %w\n", err)
+	}
+
+	walletAddr, err := walletRpc.WalletNew(ctx, vTypes.KTBLS)
+	if err != nil {
+		return address.Undef, fmt.Errorf("create wallet failed: %w\n", err)
+	}
+	fmt.Printf("wallet: %v\n", walletAddr)
+
+	return walletAddr, nil
+}
+
+func CreateMiner(ctx context.Context, params TestCaseParams, walletAddr address.Address) (string, error) {
+	cmd := []string{
+		"./venus-sector-manager",
+		"util",
+		"miner",
+		"create",
+		"--sector-size=8MiB",
+		"--exid=" + string(rune(rand.Intn(1000000))),
+	}
+	cmd = append(cmd, "--from="+walletAddr.String())
+
+	minerAddr, err := params.K8sEnv.ExecRemoteCmd(ctx, params.VenusSectorManagerDeployer.Pods()[0].GetName(), cmd)
+	if err != nil {
+		return "", fmt.Errorf("exec remote cmd failed: %w\n", err)
+	}
+
+	return string(minerAddr), nil
+}
+
+func SetActorAddr(ctx context.Context, params TestCaseParams, minerAddr string) (string, error) {
 	endpoint := params.VenusMarket.SvcEndpoint()
 	if env.Debug {
 		var err error
@@ -59,5 +167,6 @@ func marketListen(ctx context.Context, params TestCaseParams) (string, error) {
 	for _, peer := range addrs.Addrs {
 		fmt.Printf("%s/p2p/%s\n", peer, addrs.ID)
 	}
+
 	return "", err
 }
