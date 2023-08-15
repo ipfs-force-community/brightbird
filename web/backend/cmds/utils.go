df8ce7c5ec2d36e4efc814074df84fe1cfd47e54
hunjixin <1084400399@qq.com>
2023-08-15 14:36:38 +0800

    feat: support set listen env for cli

--- web/backend/cmds/utils.go
+++ web/backend/cmds/utils.go
@@ -1,10 +1,13 @@
 package cmds
 
-import "github.com/ipfs-force-community/brightbird/web/backend/client"
+import (
+	"github.com/ipfs-force-community/brightbird/web/backend/client"
+	"github.com/urfave/cli/v2"
+)
 
-func DefaulAPI() *client.BrightBirdAPI {
+func DefaulAPI(cliCtx *cli.Context) *client.BrightBirdAPI {
 	return client.NewHTTPClientWithConfig(nil, &client.TransportConfig{
-		Host:     "127.0.0.1:12356",
+		Host:     cliCtx.String("listen"),
 		BasePath: "/api/v1",
 		Schemes:  []string{"http"},
 	})
