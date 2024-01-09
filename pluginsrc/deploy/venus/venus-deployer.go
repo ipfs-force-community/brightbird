201ff2743dacd149e9e429997164f639e7e3d2b5
andream7 <2236407598@qq.com>
2024-01-09 17:52:44 +0800

    feat: update workflow

--- pluginsrc/deploy/venus/venus-deployer.go
+++ pluginsrc/deploy/venus/venus-deployer.go
@@ -75,7 +75,7 @@ func DeployFromConfig(ctx context.Context, k8sEnv *env.K8sEnvDeployer, incomineC
 	}
 
 	if len(incomineCfg.GenesisStorage) > 0 {
-		args = append(args, "--genesisfile=/root/devgen/devgen.car")
+		args = append(args, "--genesisfile=/shared-dir/genesisnode/dev.gen")
 	}
 
 	if len(incomineCfg.SnapshotStorage) > 0 {
